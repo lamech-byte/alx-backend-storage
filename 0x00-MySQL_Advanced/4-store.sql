5fa76b644f7101a8c78b89c98b41219c79de4b2c
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-13 14:30:43 +0300

    4-store.sql

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- Create a trigger to decrease quantity after adding an order
+DELIMITER $$
+CREATE TRIGGER decrease_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END$$
+DELIMITER ;
