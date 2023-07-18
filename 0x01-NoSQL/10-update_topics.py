293d9b841d7eec2845f8449b4640b4d4051f9fde
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-18 15:52:44 +0300

    10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -15,4 +15,7 @@ def update_topics(mongo_collection, name, topics):
         name (str): The school name to update.
         topics (List[str]): The list of topics approached in the school.
     """
-    mongo_collection.update_many({ "name": name }, { "$set": { "topics": topics } })
+    mongo_collection.update_many(
+        { "name": name }, 
+        { "$set": { "topics": topics}}
+    )
