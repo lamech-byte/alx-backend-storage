0f54a13c37d627a7f0dce18e895f1bcb3055a0bf
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-18 14:58:23 +0300

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,16 @@
-def list_all(mongo_collection):
+#!/usr/bin/env python3
+""" List all documents in a collection """
+
+from pymongo.collection import Collection
+from typing import List
+
+
+def list_all(mongo_collection: Collection) -> List[dict]:
     """
     List all documents in a MongoDB collection.
 
     Args:
-        mongo_collection (pymongo.collection.Collection):
-        The pymongo collection object.
+        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
 
     Returns:
         list: A list containing all the documents in the collection.
