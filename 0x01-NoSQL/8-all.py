cd5a5b5f841ec5b61822d449b85b4dc6d4b4769b
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-18 14:40:15 +0300

    8-all.py

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+def list_all(mongo_collection):
+    """
+    List all documents in a MongoDB collection.
+
+    Args:
+        mongo_collection (pymongo.collection.Collection): The pymongo collection object.
+
+    Returns:
+        list: A list containing all the documents in the collection.
+    """
+    documents = list(mongo_collection.find({}))
+    return documents
