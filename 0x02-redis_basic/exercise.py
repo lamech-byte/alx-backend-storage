4d2e46adf339472da7118f3c154b45cac104f296
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-19 18:26:03 +0300

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,16 @@
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        if isinstance(data, (str, bytes)):
+            self._redis.set(key, data)
+        elif isinstance(data, (int, float)):
+            self._redis.set(key, str(data))
+        return key
