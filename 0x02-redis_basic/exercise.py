151168e4542841bb6cd7db71f5aead84866f0b7d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-19 18:35:14 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,6 @@
 import redis
 import uuid
-from typing import Union
-
+from typing import Callable, Union
 
 class Cache:
     def __init__(self):
@@ -15,3 +14,17 @@ class Cache:
         elif isinstance(data, (int, float)):
             self._redis.set(key, str(data))
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, fn=int)
