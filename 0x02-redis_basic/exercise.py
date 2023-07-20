e12cd7e90da3a915a69b22c7db830dcffc004c61
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-20 09:12:43 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,30 +4,34 @@ import redis
 import uuid
 
 class Cache:
-def __init__(self):
-self._redis = redis.Redis()
+    def __init__(self):
+        self._redis = redis.Redis()
 
-def store(self, data):
-# Generate a random key
-key = str(uuid.uuid4())
-# Store the data as a byte string
-self._redis.set(key, data)
-# Return the key
-return key
+    def store(self, data):
+        # Generate a random key
+        key = str(uuid.uuid4())
+        # Store the data as a byte string
+        self._redis.set(key, data)
+        # Return the key
+        return key
 
-def get(self, key, fn=None):
-# Get the data from the key
-data = self._redis.getkey
-# If fn is provided, apply it to the data
-if fn:
-data = fn(data)
-# Return the data
-return data
+    def get(self, key, fn=None):
+        # Get the data from the key
+        data = self._redis.get(key)
+        # If key does not exist, return None
+        if data is None:
+            return None
+        # If fn is provided, apply it to the data
+        if fn:
+            data = fn(data)
+        # Return the data
+        return data
 
-def get_str(self, key):
-# Get the data as a UTF-8 string
-return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key):
+        # Get the data as a UTF-8 string
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
-def get_int(self, key):
-# Get the data as an integer
-return self.get(key, fn=int)
+    def get_int(self, key):
+        # Get the data as an integer
+        return self.get(key, fn=int)
+
