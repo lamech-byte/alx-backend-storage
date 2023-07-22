b9b9721451311cec6bb0d640c4ce7eb39eb83db7
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-22 23:15:47 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -92,6 +92,7 @@ if __name__ == "__main__":
     key1 = cache.store("Data1")
     key2 = cache.store("Data2")
     key3 = cache.store("Data3")
-
     
-    replay(cache, cache.store)  # Pass both the cache object and the method to replay
+    
+    replay(cache, cache.store)
+    # Pass both the cache object and the method to replay
