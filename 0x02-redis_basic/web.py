45647d8686252a3e482cdb0acd7e786e6f2d718d
Lamech M <79203512+lamech-byte@users.noreply.github.com>
2023-07-23 11:39:47 +0300

    web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -14,25 +14,24 @@ def get_page(url: str) -> str:
     url_count = r.get(count_key)
 
     # If the URL has not been accessed before,
-    # set the count to 0 with an expiration time of 10 seconds
+    # set the count to 1 with an expiration time of 10 seconds
     if url_count is None:
-        r.setex(count_key, 10, 0)
-        url_count = b'0'  # Convert to bytes to match the return type
-
-    # Increment the count and update the expiration time
-    r.incr(count_key)
-    r.expire(count_key, 10)
+        r.setex(count_key, 10, 1)
+    else:
+        # If the URL has been accessed before,
+        # increment the count and update the expiration time
+        r.incr(count_key)
+        r.expire(count_key, 10)
 
     # Fetch the page content using requests
     response = requests.get(url)
-    page_content = response.text
+    page_content = response.text  # Extract only the page content
 
-    return page_content, url_count.decode()
-
+    return page_content
 
 if __name__ == "__main__":
     url = "http://google.com"
-    page_content, url_count = get_page(url)
-    print(f"{url} is cached for 10 seconds from the redis cache after 10 sec")
+    page_content = get_page(url)
+    print(f"{url} is cached for 10 seconds and removed
+    from the redis cache after 10 sec")
     print(f"Page Content: {page_content}")
-    print(f"URL Count: {url_count}")
